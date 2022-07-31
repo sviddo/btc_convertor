f43c90dd153c6eaea283fccebe7e22174ac4c16a
sviddo <snisarvlad04@gmail.com>
2022-07-31 16:58:04 +0300

    add template to fill the email form in browser

--- btc_convertor/settings.py
+++ btc_convertor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ovhggwb7%gz9l!&xlyy75(8=t@l4dr8nw8lf#%2_=ye^yv(zs(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'btc_convertor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
